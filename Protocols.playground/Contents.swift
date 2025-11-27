8d5df0f1beff2661e70287a2547b96aa3c91a042
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-27 19:34:26 +0530

    Basics of protocol - property & method req, class only protocols, mutating req

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,3 +1,102 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Protocols
+ Define requirements that conforming types must implement.
+ */
+
+protocol SomeProtocol {
+    var text: String { get }
+    var number: Int { get set }
+}
+
+// Above protocol when conformed by any type (Class, Struct, or Enum)
+class SomeClass: SomeProtocol {
+    // We can declare text property as constant (let) as it is just gettable property
+    let text: String = ""
+    
+    // We cannot declare number property as constant, as it is get-set property
+    var number: Int = 0
+}
+
+
+class SuperClass { }
+
+// When a BaseClass which has a SuperClass wants to conform to a protocol, it should be written like this, First -> inherit superclass, second -> conform to protocol
+class BaseClass: SuperClass, SomeProtocol {
+    var text: String = ""
+    var number: Int = 0
+}
+
+// Protocol with Type property
+protocol AnotherProtocol {
+    static var someTypeProperty: String { get }
+}
+
+// Using protocol property req as computed property
+class Table: SomeProtocol {
+    
+    var text: String {
+        return "Text"
+    }
+    
+    var number: Int
+    
+    init(number: Int) {
+        self.number = number
+    }
+}
+
+// ========= METHOD REQUIREMENTS ======================
+
+protocol MethodProtocol {
+    func someMethod()
+    func someMethodWithParameter(param: String)
+    func someMethodWithReturnType() -> String
+    
+    // Type method
+    static func someTypeMethod()
+}
+
+// Can only be conformed by classes
+protocol classOnlyProtocol: AnyObject {
+    func someClassOnlyMethod()
+}
+
+// Error :- Non-class type 'Stack' cannot conform to class protocol 'classOnlyProtocol'
+//struct Stack: classOnlyProtocol {
+//    
+//}
+
+// ========= MUTATING METHOD REQUIREMENTS ======================
+// mutating indicates that the method is allowed to modify the instance it belongs to and any properties of that instance.
+// In context of protocol, if you want any conforming value type to mutate its instances, then mark the protocol function mutating
+// Same is not true when classes conform to protocol with mutating func, you don't require mutating keyword for its implementation
+
+// IMP -----
+// mutating is needed because structs modify the value itself, not an object.
+// Classes mutate an object on the heap, not the variable → no permission needed.
+
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum Toggle: Togglable {
+    case on, off
+    
+    mutating func toggle() {
+        switch self {
+        case .on:
+            self = .on
+        case .off:
+            self = .off
+        }
+    }
+}
+
+class TogglableClass: Togglable {
+    
+    func toggle() {
+        
+    }
+}
