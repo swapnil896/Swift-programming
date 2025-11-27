f1616911dc4d1b90c2ca5010ee85f4f7b5a403d6
Swapnil Magar <magarswapnil896@gmail.com>
2025-11-27 21:37:06 +0530

    Delegation pattern using protocols

--- Protocols.playground/Pages/Delegate.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Delegate.xcplaygroundpage/Contents.swift
@@ -1,4 +1,50 @@
 import Foundation
 
-var greeting = "Hello, playground"
+// ======= DELEGATION PATTERN USING PROTOCOL ==================
 
+protocol GameDelegate: AnyObject {
+    func gameDidStart()
+    func gameDidEnd()
+    func gameDidUpdateScore(score: Int)
+}
+
+class DiceGame {
+    
+    weak var delegate: GameDelegate?
+    
+    func startGame() {
+        delegate?.gameDidStart()
+    }
+    
+    func endGame() {
+        delegate?.gameDidEnd()
+    }
+    
+    func updateScore() {
+        delegate?.gameDidUpdateScore(score: 100)
+    }
+}
+
+class GameController: GameDelegate {
+    
+    init() {
+        let diceGame = DiceGame()
+        diceGame.delegate = self
+    }
+    
+    func gameDidStart() {
+        print("Dice game has started")
+    }
+    
+    func gameDidEnd() {
+        print("Dice game has ended")
+    }
+    
+    func gameDidUpdateScore(score: Int) {
+        print("Dice game updated with score : \(score)")
+    }
+    
+    
+}
+
+let diceGameController = GameController()
